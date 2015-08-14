51b625ecbc168fafc729195d58791905d5ab09f2
a25patel <aanaletap2510@gmail.com>
2015-08-14 15:54:18 -0600

    refactor to promises!

--- routes/index.js
+++ routes/index.js
@@ -6,40 +6,44 @@ var locations = db.get('locations')
 var users = db.get('users')
 
 router.get('/', function(req, res, next) {
-  meetups.find({}, function (err, documents) {
-    if(err) throw err
+  meetups.find({})
+.then(function (documents) {
     res.render('index', { meetups: documents });
   })
 });
 
+// goal
+// router.get('/meetups/:id', function(req, res, next){
+//   getSomeData(req.params.id).then(function(result){
+//     res.render('show', obj)
+//   })
+// })
+
 router.get('/meetups/:id', function(req, res, next){
-  meetups.findOne({_id:req.params.id})
-.then(function(meetup){
-  locations.findOne({_id:meetup.locationId})
-.then(function(location){
-  users.find({_id:{$in:meetup.memberIds}})
-.then(function(members){
-  var followersArray = [];
-  members.forEach(function(members) {
-    followersArray = followersArray.concat(members.follows);
-  })
-    users.find({follows:{$in:[meetup._id]}}, function(err, followers){
-      var meetupsArray = [];
-      followers.forEach(function(followers){
-        meetupsArray = meetupsArray.concat(followers.follows);
-      })
+  meetups.findOne({_id:req.params.id}).then(function(meetup){
+    locations.findOne({_id:meetup.locationId}).then(function(location){
+      users.find({_id:{$in:meetup.memberIds}}).then(function(members){
+        var followersArray = [];
+        members.forEach(function(members) {
+          followersArray = followersArray.concat(members.follows);
+        })
+        users.find({follows:{$in:[meetup._id]}}).then(function(followers){
+          var meetupsArray = [];
+          followers.forEach(function(followers){
+            meetupsArray = meetupsArray.concat(followers.follows);
+          })
 
           meetupsArray = meetupsArray.concat(followersArray);
           meetupsArray= meetupsArray.filter(function(ele){
             return ele != req.params.id
           })
-          meetups.find({_id:{$in: meetupsArray}}, function(err, followerMeetups){
+          meetups.find({_id:{$in: meetupsArray}}).then(function(followerMeetups){
             res.render('show', {
-            documents: meetup,
-            location: location,
-            member: members,
-            follower: followers,
-            followerMeetup: followerMeetups
+              documents: meetup,
+              location: location,
+              member: members,
+              follower: followers,
+              followerMeetup: followerMeetups
             })
           })
         })
