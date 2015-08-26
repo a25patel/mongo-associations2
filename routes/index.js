10be41b35813a86607c0b428874f0ec6adb1fb37
a25patel <aanaletap2510@gmail.com>
2015-08-26 11:30:39 -0600

    meetup and location complete

--- routes/index.js
+++ routes/index.js
@@ -12,45 +12,19 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/meetups/:id', function(req,res,next){
+  meetups.findOne({_id:req.params.id}, function(err, oneMeetup){
+    locations.findOne({_id:oneMeetup.locationId}, function(err, oneLocation){
+      res.render('show', {
+        meetup: oneMeetup,
+        location: oneLocation
 
+    })
+    })
+  })
+})
 
 module.exports = router;
-
-
-
-
-// var calls = require('../lib/mongo.js')
-//
-// router.get('/', function(req, res, next) {
-//   meetups.find({})
-// .then(function (documents) {
-//     res.render('index', { meetups: documents });
-//   })
-// });
-//
-// // router.get('/meetups/:id', function(req, res, next){
-// //   calls.findMeetup(req.params.id).
-// //   then(calls.addMeetupLocation).
-// //   then(calls.addMeetupMembers).
-// //   then(calls.addMeetupFollowers).
-// //   then(calls.addMeetupsFollowed).then(function(master) {
-// //     console.log(master);
-// //     res.render('show', master)
-// //   })
-// // });
-//
-// module.exports = router;
-
-
-// goal
-// router.get('/meetups/:id', function(req, res, next){
-//   getSomeData(req.params.id).then(function(result){
-//     res.render('show', obj)
-//   })
-// })
-// $in needs to have something in an array!
-
-
 
 // router.get('/meetups/:id', function(req, res, next){
 //   meetups.findOne({_id:req.params.id}).then(function(meetup){
@@ -84,3 +58,23 @@ module.exports = router;
 //     })
 //   })
 // });
+
+
+// var calls = require('../lib/mongo.js')
+//
+// // router.get('/meetups/:id', function(req, res, next){
+// //   calls.findMeetup(req.params.id).
+// //   then(calls.addMeetupLocation).
+// //   then(calls.addMeetupMembers).
+// //   then(calls.addMeetupFollowers).
+// //   then(calls.addMeetupsFollowed).then(function(master) {
+// //     console.log(master);
+// //     res.render('show', master)
+// //   })
+// // });
+
+// router.get('/meetups/:id', function(req, res, next){
+//   getSomeData(req.params.id).then(function(result){
+//     res.render('show', obj)
+//   })
+// })
