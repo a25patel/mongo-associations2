959e4914903114f6064b936d921a61c31cc1ccb3
a25patel <aanaletap2510@gmail.com>
2015-08-26 14:12:53 -0600

    start over. round 6

--- routes/index.js
+++ routes/index.js
@@ -12,63 +12,8 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/meetups/:id', function(req,res,next){
-  meetups.findOne({_id: req.params.id}, function(err, oneMeetup){
-    locations.findOne({_id: oneMeetup.locationId}, function(err, oneLocation){
-      users.find({_id:{$in: oneMeetup.memberIds}}, function(err, members){
-        memArr = [];
-        members.forEach(function(members){
-          memArr = memArr.concat(members.follows);
-        })
-        users.find({follows:{$in:[oneMeetup._id]}}, function(err, followers){
-          follArr = [];
-          followers.forEach(function(followers){
-            follArr = follArr.concat(followers.follows)
-          })
-          memArr = memArr.concat(follArr);
-          memArr = memArr.filter(function(element){
-            return element != req.params.id
-          })
-          meetups.find({_id:{$in: memArr}}, function(err, meetupsFollowed){
-            res.render('show', {
-              meetup: oneMeetup,
-              location: oneLocation,
-              members: members,
-              followers: followers,
-              meetupsFollowed: meetupsFollowed
-            })
-          })
-        })
-      })
-    })
-  })
-});
 
 module.exports = router;
-
-//         users.find({follows:{$in:[oneMeetup._id]}}, function(err, followers){
-//           followersArray = [];
-//           followers.forEach(function(followers){
-//             followersArray = followersArray.concat(followers.follows)
-//           })
-//           followersArray = followersArray.concat(membersArray)
-//           followersArray = followersArray.filter(function(element){
-//             return element != req.params.id
-//           })
-//           meetups.find({_id:{$in:followersArray}}, function(err, meetupsFollowed){
-//             res.render('show', {
-//               meetup: oneMeetup,
-//               location: oneLocation,
-//               members: members,
-//               follower: followers,
-//               followerMeetup: meetupsFollowed
-//             })
-//           })
-//         })
-//       })
-//     })
-//   })
-// })
 
 
 // var calls = require('../lib/mongo.js')
