604efbebfe9f3e906c4e85f81c4303a793ff0150
a25patel <aanaletap2510@gmail.com>
2015-08-29 15:09:35 -0600

    refactoring in progress

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var db = require('monk')('localhost/mongo-associations-lesson')
 var meetups = db.get('meetups')
 var locations = db.get('locations')
 var users = db.get('users')
+var calls = require('../lib/mongo.js')
 
 router.get('/', function(req, res, next) {
   meetups.find({}, function (err, documents) {
@@ -13,42 +14,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/meetups/:id', function(req,res,next){
-  meetups.findOne({_id: req.params.id}, function(err, oneMeetup){
-    locations.findOne({_id: oneMeetup.locationId}, function(err, oneLocation){
-      users.find({_id:{$in: oneMeetup.memberIds}}, function(err, members){
-        array1 = [];
-        members.forEach(function(members){
-          array1 = array1.concat(members.follows)
-        })
-        users.find({follows:{$in:[oneMeetup._id]}}, function(err, followers){
-          array2 = [];
-          followers.forEach(function(followers){
-            array2 = array2.concat(followers.follows)
-          })
-           array2 = array2.concat(array1)
-           array2 = array2.filter(function(elements){
-             return elements != req.params.id
-           })
-           meetups.find({_id:{$in: array2}}, function(err, followedMeetups){
-            res.render('show', {
-              meetup: oneMeetup,
-              location: oneLocation,
-              members: members,
-              followers: followers,
-              followedMeetups: followedMeetups
-
-           })
-
-        })
-      })
-    })
-    })
+  calls(req.params.id).then(function(master){
+    console.log(master);
+    res.render('show', master)
   })
 })
 module.exports = router;
 
 
-// var calls = require('../lib/mongo.js')
 //
 // // router.get('/meetups/:id', function(req, res, next){
 // //   calls.findMeetup(req.params.id).
